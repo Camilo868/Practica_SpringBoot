848efe9a97e22a31b41f16e85fab09bff2b76eac
Camilo868 <camiloramirezosorio12345@gmail.com>
2026-09-21 23:38:38 -0500

    Limpiar repositorio: eliminar carpeta practica duplicada

--- practica/src/main/java/org/example/practica/exceptions/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package org.example.practica.exceptions;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
--- practica/src/main/java/org/example/practica/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.practica.repository;
-
-
-import org.example.practica.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-    boolean existByname(String name);
-}
